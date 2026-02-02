79a041362035dcd1a0397cba95b03ecf626d61e4
Md. anamul Hasan Rony <u1804118@student.cuet.ac.bd>
2026-02-02 16:16:33 +0600

    Order and Order History Frontend

---
+++ foodappfrontend/src/pages/OrderHistory/OrderHistory.jsx
@@ -0,0 +1,99 @@
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContex';
+import'./OrderHistory.css'
+import { getOrderHistory } from '../../service/OrderService';
+
+const OrderHistory = () => {
+    const {token}=useContext(StoreContext);
+    const [message,setMessage]=useState("verifying payment..");
+
+    const [orderHistoryData, setOrderHistoryData]=useState([]);
+    
+    useEffect(()=>{
+        const loadAllOrderHistory=async()=>{
+            
+        const res= await getOrderHistory(token);
+        console.log(res.data);
+        setOrderHistoryData(res.data);
+           
+    };
+        loadAllOrderHistory();
+
+    },[]);
+
+  return (
+    <div>
+      <div className="container mt-4">
+    <div className="p-5 mb-4 bg-light rounded-3">
+      <h2 className='text-center display-5 fw-bold'>Order History</h2>
+      <hr/>
+    </div>
+
+        <table className="table table-bordered table-hover table-striped align-middle">
+        <thead className="table-light">
+            <tr className="sticky-top">
+            <th>OrderId</th>
+            <th>Customer Name</th>
+            <th>Customer Phone No</th>
+            <th>Delivery Address</th>
+            <th>FoodItems</th>
+            <th>Unit Price</th>
+            <th>Quantity</th>
+            <th>Total Price</th>
+            <th>Payment Status</th>
+            <th>Order Status</th>
+            </tr>
+        </thead>
+        <tbody>
+           {orderHistoryData.length === 0 && (
+                <tr>
+                <td colSpan="10" className="text-center text-muted">
+                    No orders found
+                </td>
+                </tr>
+            )}
+
+             {orderHistoryData.map(order =>
+                order.orderedItem.map((item, index) => (
+                <tr key={`${order.id}-${index}`}>
+
+                    {index === 0 && (
+                    <>
+                        <td rowSpan={order.orderedItem.length}>{order.id}</td>
+                        <td rowSpan={order.orderedItem.length}>{order.customerName}</td>
+                        <td rowSpan={order.orderedItem.length}>{order.customerPhoneNo}</td>
+                        <td rowSpan={order.orderedItem.length}>
+                        {order.customerAddress}, {order.customerCity}, {order.customerState}, {order.customerCountry}
+                        </td>
+                    </>
+                    )}
+                    <td>{item.name}</td>
+                    <td>৳{item.price}</td>
+                    <td>{item.quantity}</td>
+                    {index === 0 && (
+                        <>
+                            <td rowSpan={order.orderedItem.length}>৳{order.totalAmount}</td>
+                            <td rowSpan={order.orderedItem.length} className={order.paymentStatus === "PAID" ?"text-success fw-bold": (order.paymentStatus === "FAILED" ?"text-danger fw-bold":"text-warning fw-bold")}>{order.paymentStatus}</td>
+                            <td rowSpan={order.orderedItem.length} className={order.orderStatus === "DELIVERED" ? "text-success fw-bold" : (order.orderStatus === "FAILED" ?"text-danger fw-bold":"text-warning fw-bold")}>{order.orderStatus}</td>
+                        </>
+                    )}
+                   
+                </tr>
+                ))
+            )}
+            
+        </tbody>
+        </table>
+
+  </div>
+    </div>
+  )
+}
+
+export default OrderHistory;
+// ngrok http 8080 --request-header-add "ngrok-skip-browser-warning: true"
+
+///tmole 8080
+//
---
+++ foodappfrontend/src/service/OrderService.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+// const DOMAIN=import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+const DOMAIN='';
+
+const API_URL=DOMAIN+'/api/order';
+export const createOrderWithPayment=async(token,data)=>{
+
+     try {   
+            const res = await axios.post(API_URL+"/create", data,{headers: { Authorization: `Bearer ${token}` }});
+            return res;
+            
+
+            } catch (err) {
+                    throw err;
+                }
+}
+
+export const getOrderHistory=async(token)=>{
+
+     try {   
+            const res = await axios.get(API_URL,{ headers: { Authorization: `Bearer ${token}`}});
+            return res;
+            
+
+            } catch (err) {
+                    throw err;
+                }
+}
+
+   
